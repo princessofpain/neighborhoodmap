dad783270c3f3d6e240adc93cc626df9433cd6a7
Chilja Speransky <magiclanzelot@gmx.net>
2018-07-13 10:46:31 +0200

    Map Markers Array in the Google Map

--- src/Map.js
+++ src/Map.js
@@ -5,10 +5,24 @@ import Markers from './Markers';
 export default class Map extends Component {
   static defaultProps = {
     center: { lat: 52.520645, lng: 13.409779},
-    zoom: 11
+    zoom: 11,
+    locations: [
+      {title: 'Chay Umi', lng: 13.414179, lat: 52.547450, details: 'abc'},
+      {title: 'Goodies', lng: 13.214402, lat: 52.439586, details: 'abc'},
+      {title: 'Dolores', lng: 13.343829, lat: 52.501156, details: 'abc'},
+      {title: 'Peter Pane', lng: 13.320434, lat: 52.457234, details: 'abc'},
+      {title: 'Satyam', lng: 13.322033, lat: 52.508920, details: 'abc'},
+      {title: 'Chay Village', lng: 13.348840, lat: 52.489857, details: 'abc'}
+    ]
   }
 
-  showDetails() = {
+  // setMarkers = () => {
+  //   this.props.locations.forEach((location) => {
+  //     <Markers title={location.title} lat={location.lat} lng={location.lng}/>
+  //   });
+  // }
+
+  showDetails() {
 
   }
 
@@ -21,37 +35,55 @@ export default class Map extends Component {
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyADx7MTThdOIoeAzBqNRIM8MdPJqOkP87Y' }}
           defaultCenter={ this.props.center }
-          defaultZoom={ this.props.zoom }>
+          defaultZoom={ this.props.zoom }
+        >
+        {this.props.locations.map((location) => {
+          return(
+            <Markers
+              title = {location.title}
+              lng = {location.lng}
+              lat = {location.lat}
+              details = {location.details}
+            />
+          )
+        })}
+
+{/*          <Markers
+             lat={52.547450}
+             lng={13.414179}
+             text={'Chay Umi'}
+             info={this.showDetails()}
+           />
+           <Markers
+             lat={52.439586}
+             lng={13.214402}
+             text={'Goodies'}
+             info={this.showDetails()}
+           />
+           <Markers
+             lat={52.501156}
+             lng={13.343829}
+             text={'Dolores'}
+             info={this.showDetails()}
+           />
+           <Markers
+             lat={52.457234}
+             lng={13.320434}
+             text={'Peter Pane'}
+             info={this.showDetails()}
+           />
           <Markers
-            lat={52.547450}
-            lng={13.414179}
-            text={'Chay Umi'}
-          />
-          <Markers
-            lat={52.439586}
-            lng={13.214402}
-            text={'Goodies'}
-          />
-          <Markers
-            lat={52.501156}
-            lng={13.343829}
-            text={'Dolores'}
-          />
-          <Markers
-            lat={52.457234}
-            lng={13.320434}
-            text={'Peter Pane'}
-          />
-         <Markers
-            lat={52.508920}
-            lng={13.322033}
-            text={'Satyam'}
-          />
-          <Markers
-            lat={52.489857}
-            lng={13.348840}
-            text={'Chay Village'}
-          />
+             lat={52.508920}
+             lng={13.322033}
+             text={'Satyam'}
+             info={this.showDetails()}
+           />
+           <Markers
+             lat={52.489857}
+             lng={13.348840}
+             text={'Chay Village'}
+             info={this.showDetails()}
+            />*/}
         </GoogleMapReact>
       </div>
     )
